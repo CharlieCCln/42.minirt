505134f5a7ee2aa5606729016053df471293bce9
Colas Gelin <cgelin@z1r3p5.42lyon.fr>
2023-08-28 15:29:58 +0200

    Implementations de fonctions de calcul de coordonnees dans le main

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccrottie <ccrottie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cgelin <cgelin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 14:47:03 by ccrottie          #+#    #+#             */
-/*   Updated: 2023/08/28 14:30:05 by ccrottie         ###   ########.fr       */
+/*   Updated: 2023/08/28 15:29:10 by cgelin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,40 +39,38 @@ int key_press(int keycode, t_data *data)
 void init_mlx(t_data *data)
 {
 	data->mlx.mlx_ptr = mlx_init();
-	mlx_get_screen_size(data->mlx.mlx_ptr, &data->mlx.size_x, &data->mlx.size_y);
-	data->mlx.size_x /= 2;
-	data->mlx.size_y /= 2;
-	data->mlx.win_ptr = mlx_new_window(data->mlx.mlx_ptr, data->mlx.size_x, data->mlx.size_y, "Window");
+	data->mlx.win_ptr = mlx_new_window(data->mlx.mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT, "Window");
 	mlx_hook(data->mlx.win_ptr, DestroyNotify, StructureNotifyMask, mlx_exit, data);
 	mlx_key_hook(data->mlx.win_ptr, key_press, data);
-	mlx_loop(data->mlx.mlx_ptr);
+}
+t_coords gen_coords (float x, float y, float z) 
+{
+	return ((t_coords){x, y, z});
 }
 
-// t_coords create_coords(t_coords p1) 
-// {
-// 	t_coords res;
+t_coords v_add (t_coords p1, t_coords p2) 
+{
+	return (gen_coords((p1.x + p2.x), (p1.y + p2.y), (p1.z + p2.z)));
+}
+
+
+t_ray create_ray(t_cam cam, float x, float y) 
+{
+	t_ray ray;
+
+	ray.origin = cam.coords;
+	(void)x; (void)y;
 	
-// 	return ()
-// }
-
-// t_coords v_add (t_coords p1, t_coords p2) 
-// {
-// 	return (create_coords(, ));
-// }
-
-// t_ray create_ray(t_cam cam, float x, float y) 
-// {
-// 	t_ray ray;
-
-// 	ray.origin = cam.coords;
-// 	ray.dir = v_add(x);
-// }
+	// printf("or : rayx = %f, rayy = %f, rayz = %f\n dir : rayx = %f, rayy = %f, rayz = %f\n", ray.origin.x, ray.origin.y, ray.origin.z, ray.dir.x, ray.dir.y, ray.dir.z);
+	return (ray);
+}
 
 void draw_rays(t_data *data) 
 {
 	int x;
 	int y;
 	t_ray ray;
+	// printf("sizex = %d, sizey = %d", data->mlx.size_y, data->mlx.size_x);
 
 	y = -1;
 	while (y++ < data->mlx.size_y - 1)
@@ -80,9 +78,18 @@ void draw_rays(t_data *data)
 		x = -1;
 		while (x++ < data->mlx.size_x - 1) 
 		{
-			// ray = create_ray(data->cam, (float)x / data->mlx.size_x, (float)y / data->mlx.size_y);
+			ray = create_ray(data->cam, (float)x / data->mlx.size_x, (float)y / data->mlx.size_y);
 		}
 	}
+}
+
+void get_cam_infos(t_data *data) 
+{
+	float ver_fov;
+	float hor_fov;
+	
+	ver_fov = 2 * tan(data->cam.fov / 2);
+	hor_fov = ver_fov * (float)data->mlx.size_y / data->mlx.size_x;
 }
 
 int	main(int argc, char **argv)
@@ -97,7 +104,9 @@ int	main(int argc, char **argv)
 	}
 	parsing_hub(&data, argv[1]);
 	init_mlx(&data);
-	draw_rays(&data);
+	get_cam_infos(&data);
+	// draw_rays(&data);
+	mlx_loop(data.mlx.mlx_ptr);
 	terminate(&data);
 	return (0);
 }
