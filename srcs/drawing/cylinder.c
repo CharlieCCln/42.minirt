e57e28cc68f2a6ca29a69c9419250919374d163e
Colas Gelin <cgelin@z4r9p5.42lyon.fr>
2023-10-02 11:16:55 +0200

    fixing cylinder that bug when we change orientation

--- srcs/drawing/cylinder.c
+++ srcs/drawing/cylinder.c
@@ -6,13 +6,13 @@
 /*   By: cgelin <cgelin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:33:18 by colas             #+#    #+#             */
-/*   Updated: 2023/09/26 19:04:03 by cgelin           ###   ########.fr       */
+/*   Updated: 2023/10/02 11:14:20 by cgelin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-double			solve_plane(t_coords o, t_coords d, t_coords plane_p, t_coords plane_nv)
+double	solve_plane(t_coords o, t_coords d, t_coords plane_p, t_coords plane_nv)
 {
 	double	x;
 	double	denom;
@@ -21,34 +21,40 @@ double			solve_plane(t_coords o, t_coords d, t_coords plane_p, t_coords plane_nv
 	if (denom == 0)
 		return (INFINITY);
 	x = (v_dot(plane_nv, v_oper(SUB, plane_p, o))) / denom;
-	return (x > EPSILON ? x : INFINITY);
+	if (x > EPSILON)
+		return (x);
+	else
+		return (INFINITY);
 }
 
 double	distance(const t_coords p1, const t_coords p2)
 {
-	return (sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2) + pow(p2.z - p1.z, 2)));
+	return (sqrt(pow(p2.x - p1.x, 2) + \
+	pow(p2.y - p1.y, 2) + pow(p2.z - p1.z, 2)));
 }
 
-static double	caps_intersection(t_ray ray, const t_object cylinder)
+static double	caps_intersection(t_ray ray, const t_object cyl)
 {
-	double	id1;
-	double	id2;
+	double		id1;
+	double		id2;
 	t_coords	ip1;
 	t_coords	ip2;
 	t_coords	c2;
 
-	c2 = v_oper(ADD, cylinder.origin, v_scale(cylinder.dir, cylinder.height));
-	id1 = solve_plane(ray.origin, ray.dir, cylinder.origin, cylinder.dir);
-	id2 = solve_plane(ray.origin, ray.dir, c2, cylinder.dir);
+	c2 = v_oper(ADD, cyl.origin, v_scale(cyl.dir, cyl.height));
+	id1 = solve_plane(ray.origin, ray.dir, cyl.origin, cyl.dir);
+	id2 = solve_plane(ray.origin, ray.dir, c2, cyl.dir);
 	if (id1 < INFINITY || id2 < INFINITY)
 	{
 		ip1 = v_oper(ADD, ray.origin, v_scale(ray.dir, id1));
 		ip2 = v_oper(ADD, ray.origin, v_scale(ray.dir, id2));
-		if ((id1 < INFINITY && distance(ip1, cylinder.origin) <= cylinder.diameter) && (id2 < INFINITY && distance(ip2, c2) <= cylinder.diameter))
+		if ((id1 < INFINITY && distance(ip1, cyl.origin) <= cyl.diameter) \
+		&& (id2 < INFINITY && distance(ip2, c2) <= cyl.diameter))
 			return (id1 < id2 ? id1 : id2);
-		else if (id1 < INFINITY && distance(ip1, cylinder.origin) <= cylinder.diameter)
+		else if (id1 < INFINITY && \
+		distance(ip1, cyl.origin) <= cyl.diameter)
 			return (id1);
-		else if (id2 < INFINITY && distance(ip2, c2) <= cylinder.diameter)
+		else if (id2 < INFINITY && distance(ip2, c2) <= cyl.diameter)
 			return (id2);
 	}
 	return (INFINITY);
@@ -64,7 +70,8 @@ static bool	solve_cylinder(double x[2], t_ray ray, const t_object cylinder)
 
 	v = v_scale(cylinder.dir, v_dot(ray.dir, cylinder.dir));
 	v = v_oper(SUB, ray.dir, v);
-	u = v_scale(cylinder.dir, v_dot(v_oper(SUB, ray.origin, cylinder.origin), cylinder.dir));
+	u = v_scale(cylinder.dir, v_dot(\
+	v_oper(SUB, ray.origin, cylinder.origin), cylinder.dir));
 	u = v_oper(SUB, v_oper(SUB, ray.origin, cylinder.origin), u);
 	a = v_dot(v, v);
 	b = 2 * v_dot(v, u);
@@ -81,7 +88,8 @@ static void		calc_cy_normal(double x2[2], t_object cylinder, double dist1, doubl
 	double	dist;
 	double	x;
 
-	if ((dist1 >= 0 && dist1 <= cylinder.height && x2[0] > EPSILON) && (dist2 >= 0 && dist2 <= cylinder.height && x2[1] > EPSILON))
+	if ((dist1 >= 0 && dist1 <= cylinder.height && x2[0] > EPSILON) \
+	&& (dist2 >= 0 && dist2 <= cylinder.height && x2[1] > EPSILON))
 	{
 		dist = x2[0] < x2[1] ? dist1 : dist2;
 		x = x2[0] < x2[1] ? x2[0] : x2[1];
@@ -97,7 +105,6 @@ static void		calc_cy_normal(double x2[2], t_object cylinder, double dist1, doubl
 		x = x2[1];
 	}
 	x2[0] = x;
-	// return (normalize(v_oper(SUB, v_oper(SUB, v_scale(ray.dir, x),	v_scale(cylinder.dir, dist)), v_oper(SUB, cylinder.origin, ray.origin))));
 }
 
 static double	cy_intersection(t_ray ray, t_object cylinder)
@@ -106,9 +113,12 @@ static double	cy_intersection(t_ray ray, t_object cylinder)
 
 	if (!solve_cylinder(x2, ray, cylinder))
 		return (INFINITY);
-	double dist1 = v_dot(cylinder.dir, v_oper(SUB, v_scale(ray.dir, x2[0]), v_oper(SUB, cylinder.origin, ray.origin)));
-	double dist2 = v_dot(cylinder.dir, v_oper(SUB, v_scale(ray.dir, x2[1]), v_oper(SUB, cylinder.origin, ray.origin)));
-	if (!((dist1 >= 0 && dist1 <= cylinder.height && x2[0] > EPSILON) || (dist2 >= 0 && dist2 <= cylinder.height && x2[0] > EPSILON)))
+	double dist1 = v_dot(cylinder.dir, v_oper(SUB, \
+	v_scale(ray.dir, x2[0]), v_oper(SUB, cylinder.origin, ray.origin)));
+	double dist2 = v_dot(cylinder.dir, v_oper(SUB, \
+	v_scale(ray.dir, x2[1]), v_oper(SUB, cylinder.origin, ray.origin)));
+	if (!((dist1 >= 0 && dist1 <= cylinder.height && x2[0] > EPSILON) \
+	|| (dist2 >= 0 && dist2 <= cylinder.height && x2[0] > EPSILON)))
 		return (INFINITY);
 	calc_cy_normal(x2, cylinder, dist1, dist2);
 	return (x2[0]);
@@ -157,13 +167,14 @@ int	intersect_cylinder(t_ray *ray, t_object *cyl)
 	hit_dist = cylinder_intersection(*ray, *cyl, &is_on_side);
 	if (ray->dist > hit_dist && hit_dist > EPSILON)
 	{
+		double length = distance((t_coords){0,0,0}, cyl->dir);
+		cyl->dir = (t_coords){cyl->dir.x / length, cyl->dir.y / length, cyl->dir.z / length};
 		ray->dist = hit_dist;
 		ray->hit = get_hit_point(ray);
+		pos = v_oper(ADD, cyl->origin, v_scale(cyl->dir, cyl->height));
 		if (is_on_side)
-		{
-			pos = v_oper(ADD, cyl->origin, v_scale(cyl->dir, cyl->height));
-			ray->hit_norm = v_oper(SUB, get_closest_point_from_line(cyl->origin, pos, ray->hit), ray->hit);
-		}
+			ray->hit_norm = v_oper(SUB, \
+			get_closest_point_from_line(cyl->origin, pos, ray->hit), ray->hit);
 		else
 			ray->hit_norm = cyl->dir;
 		if (v_dot(ray->dir, ray->hit_norm) > 0)
